f57f084ae9a8ce9ce1d0145222b04bfd3f1ada12
planB <Mac@MacBook-Pro-toutes-Ioannis.local>
2017-02-13 11:03:12 +0200

    20170213SlidersDefaults

--- RGBSliders/ViewController.swift
+++ RGBSliders/ViewController.swift
@@ -23,10 +23,21 @@ class ViewController: UIViewController {
         
         colorSquare.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
         
+        let defaults = UserDefaults.standard
+        defaults.set(redSlider.value, forKey:"red")
+        defaults.set(greenSlider.value, forKey:"green")
+        defaults.set(blueSlider.value, forKey:"blue")
+        defaults.synchronize()
     }
     
     
-    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "openColor"){
+            let newViewController = segue.destination
+            newViewController.view.backgroundColor = colorSquare.backgroundColor
+            //segue.destination.view.backgroundColor = colorSquare.backgroundColor
+        }
+    }
     
     
     
@@ -39,7 +50,10 @@ class ViewController: UIViewController {
         colorSquare.layer.borderColor = UIColor.black.cgColor
         colorSquare.layer.borderWidth = 1
         
-        
+        let defaults = UserDefaults.standard
+        redSlider.value = defaults.float(forKey:"red")
+        greenSlider.value = defaults.float(forKey: "green")
+        blueSlider.value = defaults.float(forKey: "blue")
         
         
         
